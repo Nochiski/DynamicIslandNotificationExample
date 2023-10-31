808883ea4b6a07cb835193b4e6376d781ffb6cb4
Nochiski <sangmok112@gmail.com>
2023-10-30 22:33:17 -0400

    project setup

---
+++ Notification/Notification/NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationApp.swift
+//  Notification
+//
+//  Created by Sangmok Han on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
